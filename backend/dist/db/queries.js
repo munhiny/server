d8e574ceef98bb4ae2862d89424ef37f92a60831
Edmond Yeo <edmondyeois@gmail.com>
2021-03-11 12:24:51 +1100

    front end to access backend

--- backend/dist/db/queries.js
+++ backend/dist/db/queries.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAllEmployees = void 0;
 var index_1 = require("./index");
 var getAllEmployees = function (request, response) {
-    index_1.pool.query("SELECT emp_hiredate::varchar FROM employee")
+    index_1.pool.query("SELECT e.emp_fname, e.emp_lname, j.job_description FROM employee e JOIN job j ON e.job_code = j.job_code;")
         .then(function (results) { return response.status(200).json(results.rows); })
         .catch(function (error) {
         console.log('Error: ', error);
